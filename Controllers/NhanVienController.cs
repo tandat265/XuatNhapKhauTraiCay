b2c887469a479d60ab1dad840e5a81f9963ae2a0
Ngo Tan Dat <tandat26502@gmail.com>
2024-05-20 23:57:39 +0700

    Config(NhanVien): create edit view

--- Controllers/NhanVienController.cs
+++ Controllers/NhanVienController.cs
@@ -2,6 +2,7 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using AspnetCoreMvcFull.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace AspnetCoreMvcFull.Controllers;
 
@@ -18,7 +19,7 @@ public class NhanVienController : Controller
     ViewData["lstNhanVien"] = await dbContext.NhanViens.ToListAsync();
     ViewData["lstBoPhan"] = await dbContext.BoPhans.ToListAsync();
     ViewData["lstTrangThai"] = await dbContext.TrangThaiNhanViens.ToListAsync();
-
+    ViewData["lstChucVu"] = await dbContext.ChucVus.ToListAsync();
     return View();
   }
 
@@ -26,10 +27,53 @@ public class NhanVienController : Controller
   {
     var nhanVien = await dbContext.NhanViens.Where(item => item.MaNhanVien == id).FirstOrDefaultAsync();
     ViewData["nhanVien"] = nhanVien;
+    ViewData["chucVu"] = await dbContext.ChucVus.Where(item => item.IdChucVu == nhanVien.IdChucVu).FirstOrDefaultAsync();
     ViewData["boPhan"] = await dbContext.BoPhans.Where(item => item.IdboPhan == nhanVien.IdboPhan).FirstOrDefaultAsync();
     ViewData["trangThai"] = await dbContext.TrangThaiNhanViens.Where(item => item.IdtrangThai == nhanVien.IdtrangThai).FirstOrDefaultAsync();
     return View();
   }
+
+  public async Task<IActionResult> Edit(int id)
+  {
+    var nhanVien = await dbContext.NhanViens.Where(item => item.MaNhanVien == id).FirstOrDefaultAsync();
+    ViewData["nhanVien"] = nhanVien;
+    var chucVus = await dbContext.ChucVus.ToListAsync();
+    var chucVu = new SelectList(chucVus, "IdChucVu", "ChucVu1", nhanVien.IdChucVu);
+    ViewData["lstChucVu"] = chucVu;
+    var boPhans = await dbContext.BoPhans.ToListAsync();
+    ViewData["lstBoPhan"] = new SelectList(boPhans, "IdboPhan", "TenBoPhan", nhanVien.IdboPhan);
+    var trangThais = await dbContext.TrangThaiNhanViens.ToListAsync();
+    ViewData["lstTrangThai"] = new SelectList(trangThais, "IdtrangThai", "TrangThai", nhanVien.IdtrangThai);
+    return View(nhanVien);
+  }
+
+  [HttpPost]
+  public async Task<IActionResult> Edit(int id, [Bind("MaNhanVien,TenNhanVien,IdChucVu,IdboPhan,IdtrangThai,Email,Phone,Cccd,Address")] NhanVien nhanVienEdit)
+  {
+    if (ModelState.IsValid)
+    {
+        var nhanVien = await dbContext.NhanViens.FindAsync(nhanVienEdit.MaNhanVien);
+        if (nhanVien == null)
+        {
+            return NotFound();
+        }
+        nhanVien.TenNhanVien = nhanVienEdit.TenNhanVien;
+        nhanVien.IdChucVu = nhanVienEdit.IdChucVu;
+        nhanVien.IdboPhan = nhanVienEdit.IdboPhan;
+        nhanVien.IdtrangThai = nhanVienEdit.IdtrangThai;
+        nhanVien.Email = nhanVienEdit.Email;
+        nhanVien.Phone = nhanVienEdit.Phone;
+        nhanVien.Cccd = nhanVienEdit.Cccd;
+        nhanVien.Address = nhanVienEdit.Address;
+
+        dbContext.Update(nhanVien);
+        dbContext.SaveChanges();
+        
+        return RedirectToAction("Index");
+    }
+    return View(nhanVienEdit);
+  }
+
 
 
 }
