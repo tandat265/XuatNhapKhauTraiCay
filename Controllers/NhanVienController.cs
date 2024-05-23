9fd332c8c6298543ec5c6c6aded582af7a9b4365
Ngo Tan Dat <tandat26502@gmail.com>
2024-05-23 20:45:18 +0700

    Config(nhanvien): finished

--- Controllers/NhanVienController.cs
+++ Controllers/NhanVienController.cs
@@ -67,11 +67,72 @@ public class NhanVienController : Controller
         nhanVien.Address = nhanVienEdit.Address;
 
         dbContext.Update(nhanVien);
-        dbContext.SaveChanges();
-        
+        await dbContext.SaveChangesAsync();
+        TempData["SuccessMessage"] = "Nhân viên đã được chỉnh sửa thành công.";
         return RedirectToAction("Index");
     }
     return View(nhanVienEdit);
+  }
+
+  public async Task<IActionResult> Create()
+  {
+    var chucVus = await dbContext.ChucVus.ToListAsync();
+    var chucVu = new SelectList(chucVus, "IdChucVu", "ChucVu1");
+    ViewData["lstChucVu"] = chucVu;
+    var boPhans = await dbContext.BoPhans.ToListAsync();
+    ViewData["lstBoPhan"] = new SelectList(boPhans, "IdboPhan", "TenBoPhan");
+    var trangThais = await dbContext.TrangThaiNhanViens.ToListAsync();
+    ViewData["lstTrangThai"] = new SelectList(trangThais, "IdtrangThai", "TrangThai");
+    return View();
+  }
+
+  [HttpPost]
+  public IActionResult Create([Bind("MaNhanVien,TenNhanVien,IdChucVu,IdboPhan,IdtrangThai,Email,Phone,Cccd,Address")] NhanVien nhanVienCreate)
+  {
+    if (ModelState.IsValid)
+    {
+      dbContext.Add(nhanVienCreate);
+      dbContext.SaveChanges();
+      TempData["SuccessMessage"] = "Nhân viên đã được tạo thành công.";
+      return RedirectToAction("Index");
+    }
+    return View(nhanVienCreate);
+  }
+
+  public async Task<IActionResult> Delete(int id)
+  {
+    var nhanVien = await dbContext.NhanViens.Where(item => item.MaNhanVien == id).FirstOrDefaultAsync();
+    ViewData["nhanVien"] = nhanVien;
+    var chucVus = await dbContext.ChucVus.ToListAsync();
+    var chucVu = new SelectList(chucVus, "IdChucVu", "ChucVu1", nhanVien.IdChucVu);
+    ViewData["lstChucVu"] = chucVu;
+    var boPhans = await dbContext.BoPhans.ToListAsync();
+    ViewData["lstBoPhan"] = new SelectList(boPhans, "IdboPhan", "TenBoPhan", nhanVien.IdboPhan);
+    var trangThais = await dbContext.TrangThaiNhanViens.ToListAsync();
+    ViewData["lstTrangThai"] = new SelectList(trangThais, "IdtrangThai", "TrangThai", nhanVien.IdtrangThai);
+    return View(nhanVien);
+  }
+
+  [HttpPost]
+  public async Task<IActionResult> Delete(int id, [Bind("MaNhanVien,TenNhanVien,IdChucVu,IdboPhan,IdtrangThai,Email,Phone,Cccd,Address")] NhanVien nhanVienDelete)
+  {
+    if (ModelState.IsValid)
+    {
+        var nhanVien = await dbContext.NhanViens.FindAsync(id);
+        if (nhanVien != null)
+        {
+            dbContext.NhanViens.Remove(nhanVien);
+            await dbContext.SaveChangesAsync();
+            TempData["SuccessMessage"] = "Nhân viên đã được xoá thành công.";
+        }
+        else
+        {
+            // Handle the case where the entity was not found in the database
+            ModelState.AddModelError("", "Nhân viên không tồn tại.");
+        }
+        return RedirectToAction("Index");
+    }
+    return View(nhanVienDelete);
   }
 
 
